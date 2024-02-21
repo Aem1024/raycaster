fc82150a151f8398557454de04a8ebdc4c7300d7
Aem1024 <aem11607@gmail.com>
2024-02-20 20:05:12 -0600

    Started working on ray code.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #define FOV 20
 #define RAD_TO_DEG(X) (X * 180.0 / M_PI)
 #define DEG_TO_RAD(X) (X * M_PI / 180.0)
+// 
 //TODO Add movement, tie to delta time
 double x, y;; //X and Y coordinates
 double viewAngle = 20; // View angle relative to straight up (0 degrees)
@@ -16,7 +17,7 @@ int map[4][4] = {
 	{1,0,0,1},
 	{1,1,1,1},
 };
-double findSubUnit(double x) { // Find subunit of coordinate
+double findSubUnit(double x) { // Find subunit of coordinate from bottom left
 	double xpart, xfract;
 	xfract = modf(x, &xpart);
 	double xo = xfract * SUBUNITS;
@@ -42,8 +43,13 @@ double ray2d(double angle, double x, double y) {
 	}
 	double relAngle; // Angle from nearest quad counter-clockwise 
 	relAngle = quadDir * 90 - angle; //i think this finds the angle between the nearest quad
+	double farClockAngle = 90 - relAngle;
+	double angle1 = 90 - relAngle;
 	
 	// TODO Ray math	
+	double horTri1, vertTri1, hypo; 
+	 
+
 	printf("%lf\n",relAngle); // Prints relative angle, x offset, and x coord.
 	printf("%lf\n", xo);
 	printf("%lf\n", x);
